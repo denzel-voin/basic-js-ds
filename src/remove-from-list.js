e550235ae6b328e2409abbb1bcf798275c734c05
Denis Voinovich <denzelvojn@gmail.com>
2023-04-13 11:51:06 +0300

    feat: add remove k from  list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,8 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
+function removeKFromList(l, k) {
+  if (!l) return null;
+  while (l && l.value === k) {
+    l = l.next;
+  }
+  if (!l) return null;
+  let current = l;
+  while (current.next) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return l;
   // remove line with error and write your code here
 }
 
