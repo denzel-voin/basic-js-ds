4cd5bcedf7d56e8ae4667c920d2ed28416d70a0c
Denis Voinovich <denzelvojn@gmail.com>
2023-04-13 12:34:41 +0300

    feat: add stack

--- src/stack.js
+++ src/stack.js
@@ -13,18 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.items = [];
+  }
+  push(item) {
+    this.items.push(item);
     // remove line with error and write your code here
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    if (this.items.length === 0) {
+      return undefined;
+    }
+    return this.items.pop();
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    if (this.items.length === 0) {
+      return undefined;
+    }
+    return this.items[this.items.length - 1];
     // remove line with error and write your code here
   }
 }
