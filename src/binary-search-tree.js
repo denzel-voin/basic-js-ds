047eed2b8731e6127b401d82a9f8f377ba32ff5e
Denis Voinovich <denzelvojn@gmail.com>
2023-04-13 10:59:16 +0300

    feat: add add()

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -14,8 +14,30 @@ class BinarySearchTree {
     // remove line with error and write your code here
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.rootNode) {
+      this.rootNode = newNode;
+      return;
+    }
+  
+    let currentNode = this.rootNode;
+    while (currentNode) {
+      if (data === currentNode.data) return;
+      if (data < currentNode.data) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return;
+        }
+        currentNode = currentNode.right;
+      }
+    }
     // remove line with error and write your code here
   }
 
